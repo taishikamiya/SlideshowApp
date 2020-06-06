5f47d30964f7763735c672f1d772834b5a736ba6
Taishi Kamiya <t.kamiya@edu.ciid.dk>
2020-06-06 17:54:17 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Taishi Kamiya on 2020/06/06.
+//  Copyright © 2020 taishi.kamiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
